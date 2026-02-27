d896c423cb095c3fe947026ac13c38cbce332020
a-gskrv <agskrv@gmail.com>
2026-02-27 12:10:58 +0100

    Edit model Author and migrations

--- library/models.py
+++ library/models.py
@@ -1,8 +1,28 @@
 import datetime
 
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Author(models.Model):
     name: str = models.CharField(max_length=100)
     surname: str  = models.CharField(max_length=100)
     date_for_birth: datetime.datetime  = models.DateTimeField()
+
+    profile = models.URLField(
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+
+    deleted = models.BooleanField(
+        default=False
+    )
+
+    rating = models.FloatField(
+        default=1,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10),
+        ]
+    )
+
